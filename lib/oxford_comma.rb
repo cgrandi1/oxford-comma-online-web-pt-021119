df5ff314279303345a163c32e31da7c4427b259b
astronomical-form-6016 <carltondgrandison@gmail.com>
2019-02-21 02:15:36 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,1 @@
 def oxford_comma(array) 
-["k", "i", "w", "i" ].join
-end
-["kiwi", "durian"].join(" and ")
-end 
-["kiwi", "durian", "starfruit"].insert(-2,  "and").join( "," )
-end 
-["kiwi", "durian", "starfruit", "mangos", "dragon fruits"].insert(-2,  "and").join( " , " )
-["kiwi", "durian", "starfruit", "mangos", "dragon fruits", "lychees", "pomelos"].insert(-2,  "and").join( " , " )
